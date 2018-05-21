9e0231159d97d3c8fd09c4ef063ee9388c79151a
Miraj Patel <patel_miraj@hotmail.co.uk>
2018-05-21 23:03:40 +0100

    feat: Added match and reset guess functions
    Added a match function to add any 'matched' classes in the deck.
    Created a reset guesses function to remove the 'selected' class, reset
    the counter and reset the first and second guesses.

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@ let card = document.getElementsByClassName('card');
 let cardsArray = [...card];
 const deck = document.querySelector('.deck');
 let count = 0;
+let firstGuess = '';
+let secondGuess = '';
+let previousTarget = null;
 
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -13,26 +15,63 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 };
 
 var shuffledCards = shuffle(cardsArray);
 
 for (var i = 0; i < shuffledCards.length; i++){
-    deck.innerHTML = "";
     [].forEach.call(shuffledCards, function(item) {
-        deck.appendChild(item);
+        deck.append(item);
     })
 };
 
 for (var i = 0; i < cardsArray.length; i++) {
   cardsArray[i].addEventListener('click', function(event) {
 
-  let clicked = event.target;
+  let clicked = event.currentTarget;
+
+  if (clicked === previousTarget) {return};
+
   if (count < 2) {
     count++;
+    if (count === 1) {
+      firstGuess = clicked.dataset.name;
+      clicked.classList.add('selected');
+    }
+    else {
+      secondGuess = clicked.dataset.name;
     clicked.classList.add('selected');
+    }
+    if (firstGuess !== '' && secondGuess !== '') {
+      if (firstGuess === secondGuess) {
+        match();
+        guessesReset();
+      }
+      else {
+        guessesReset();
+      }
+    }
+    previousTarget = clicked;
   }
+
+  })
+};
+
+function match() {
+  let selected = document.querySelectorAll('.selected');
+  selected.forEach(function(card) {
+    card.classList.add('match');
+  })
+};
+
+function guessesReset() {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+
+  let selected = document.querySelectorAll('.selected');
+  selected.forEach(function(card) {
+    card.classList.remove('selected');
   })
 };
