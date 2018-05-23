6f64789edb720a283fb4c588bd754f64fd686cd5
Miraj Patel <patel_miraj@hotmail.co.uk>
2018-05-23 22:02:32 +0100

    feat: Move counter and pointer events
    Added a move counter to the game where when two cards have been selected
    the move counter gets incremented by one. Added pointer events of type
    none to each card with class 'match'.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ let firstGuess = '';
 let secondGuess = '';
 let previousTarget = null;
 let delay = 1200;
+let moves = 0;
+let counter = document.querySelector('.moves');
+const stars = document.querySelectorAll('.fa-star')
 
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -23,26 +26,25 @@ var shuffledCards = shuffle(cardsArray);
 
 for (var i = 0; i < shuffledCards.length; i++){
     [].forEach.call(shuffledCards, function(item) {
-        deck.append(item);
+      deck.appendChild(item);
     })
 };
 
 for (var i = 0; i < cardsArray.length; i++) {
   cardsArray[i].addEventListener('click', function(event) {
-
   let clicked = event.currentTarget;
-
-  if (clicked === previousTarget) {return};
-
   if (count < 2) {
     count++;
     if (count === 1) {
       firstGuess = clicked.dataset.name;
-      clicked.classList.add('selected');
+      clicked.classList.add('selected', 'disabled');
     }
     else {
       secondGuess = clicked.dataset.name;
-    clicked.classList.add('selected');
+      clicked.classList.add('selected', 'disabled');
+    }
+    if (count === 2) {
+      moveCounter();
     }
     if (firstGuess !== '' && secondGuess !== '') {
       if (firstGuess === secondGuess) {
@@ -50,29 +52,55 @@ for (var i = 0; i < cardsArray.length; i++) {
         setTimeout(guessesReset, delay);
       }
       else {
+        setTimeout(enable, delay);
         setTimeout(guessesReset, delay);
       }
     }
-    previousTarget = clicked;
   }
-
   })
 };
 
 function match() {
   let selected = document.querySelectorAll('.selected');
   selected.forEach(function(card) {
-    card.classList.add('match');
+  card.classList.add('match');
   })
+};
+
+function moveCounter() {
+  moves++;
+  counter.innerHTML = moves;
+
+  if (moves > 8 && moves < 12) {
+      for (var i = 0; i < stars.length; i++) {
+        if (i > 1) {
+          stars[i].style.visibility = 'collapse'
+        }
+
+      }
+  }
+  else if (moves > 15) {
+    for (var i = 0; i < stars.length; i++) {
+      if (i > 0) {
+        stars[i].style.visibility = 'collapse'
+      }
+    }
+  }
 };
 
 function guessesReset() {
   firstGuess = '';
   secondGuess = '';
   count = 0;
-
   let selected = document.querySelectorAll('.selected');
   selected.forEach(function(card) {
-    card.classList.remove('selected');
+  card.classList.remove('selected');
   })
 };
+
+function enable() {
+  let selected = document.querySelectorAll('.selected');
+  selected.forEach(function(card) {
+  card.classList.remove('disabled');
+  })
+}
